54d9121aa426acff5caf5747bf009695bafea251
pythongeek88 <43302108+pythongeek88@users.noreply.github.com>
2022-05-10 16:41:54 +0300

    Registered Appointment model on admin site

--- vetoffice/admin.py
+++ vetoffice/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Owner, Patient
+from .models import Owner, Patient, Appointment
 
 # Register your models here.
 admin.site.register(Owner)
 admin.site.register(Patient)
+admin.site.register(Appointment)
